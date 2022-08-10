102c3ae27fd2afb93effd55ae0feb5710c95be93
Moe <m.ibrahim0228@gmail.com>
2022-08-10 13:20:58 -0400

    added dropdown menu for selecting different cryptos

--- SpringUnitTesting/src/main/java/com/example/SpringUnitTesting/PriceController.java
+++ SpringUnitTesting/src/main/java/com/example/SpringUnitTesting/PriceController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 public class PriceController {
 
-    private CryptoService cryptoService;
+    private final CryptoService cryptoService;
 
     public PriceController(CryptoService cryptoService) {
         this.cryptoService = cryptoService;
@@ -15,9 +15,20 @@ public class PriceController {
 
     @GetMapping("/crypto/{crypto}")
     public String price(@PathVariable String crypto) {
-        return String.format("<h1>Hello User</h1>" +
-                "Price of "+ crypto+ " is currently: " + cryptoService.getCryptoPrice(crypto));
+        StringBuilder coinPage = new StringBuilder();
+        coinPage.append("<h2>Price of " + crypto + " is currently: " + cryptoService.getCryptoPrice(crypto) + "<br></h2>");
+        coinPage.append("<select name=\"forma\" onchange=\"location = this.value;\" style=\"padding: 40px; background-color:paleturquoise; border:none;\"> ");
+        coinPage.append("<option value=\"http://localhost:8080/\">(select an option)</option>");
+        coinPage.append("<option value=\"http://localhost:8080/\">Home</option>");
+        coinPage.append("<option value=\"http://localhost:8080/crypto/bitcoin\">Bitcoin</option>)");
+        coinPage.append("<option value=\"http://localhost:8080/crypto/usd-coin\">USD-COIN</option>");
+        coinPage.append("<option value=\"http://localhost:8080/crypto/tether\">Tether</option>");
+        coinPage.append("<option value=\"http://localhost:8080/crypto/ethereum\">Ethereum</option>");
+        coinPage.append("<option value=\"http://localhost:8080/crypto/cardano\">Cardano</option>");
+        coinPage.append("<option value=\"http://localhost:8080/crypto/solana\">Solana</option></select>");
 
+
+        return coinPage.toString();
     }
 
 }
